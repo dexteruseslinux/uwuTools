16359b1bce7fa46bc8cbb7088fd552fc2b4a51a6
Breadtard <breadboithetard@gmail.com>
2021-01-20 21:30:18 +0100

    added colors and fixed segfault

--- uwuShell/main.c
+++ uwuShell/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "colors.h"
 #include <unistd.h> // for getting env vars and stuff
 #include <stdlib.h>
 #include <string.h>
@@ -10,10 +11,10 @@ int main(void) {
     char* currentDir = getcwd(currentDir, sizeof(currentDir));
     char* user = getenv("USER");         // yoink the username
     printf("Welcome to uwuShell, %s\n", user);
-    char* cmd = "";                 // command we executing
-    char* lastcmd = "";             // previous command (used for !! bangers)
+    char* cmd[2048];                 // command we executing
+    char* lastcmd[2048];             // previous command (used for !! bangers)
     while (1) {
-        printf("%s > %s => ", user, currentDir);
+        printf(BBLU "%s >" reset BLU " %s => " reset, user, currentDir);
         scanf("%s", cmd);           // read the command
         if (strcmp(cmd, "!!") == 0) {          // if command is !!
             strcpy(cmd, lastcmd);   // make the current command the last command
