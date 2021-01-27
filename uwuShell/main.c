6a47bc2dd677978237681f5986e41e7a54e1c66e
Breadtard <breadboithetard@gmail.com>
2021-01-27 17:43:39 +0100

    arguments work now pog

--- uwuShell/main.c
+++ uwuShell/main.c
@@ -4,21 +4,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include "uwuShell-commands.h"
-#define true = 0;
-#define false = !0;
 
 int main(void) {
-    char* currentDir = getcwd(currentDir, sizeof(currentDir));
+    char* currentDir = getcwd(currentDir, sizeof(currentDir)); // FIX-ME: this doesnt fucking work
     char* user = getenv("USER");         // yoink the username
     printf("Welcome to uwuShell, %s\n", user);
-    char* cmd[2048];                 // command we executing
-    char* lastcmd[2048];             // previous command (used for !! bangers)
+    char *cmd[2048];                 // command we executing
+    char *lastcmd[2048];             // previous command (used for !! bangers)
     while (1) {
         printf(BBLU "%s >" reset BLU " %s => " reset, user, currentDir);
-        scanf("%s", cmd);           // read the command
+        gets(cmd, 2048, stdin);                // this will probably wail
         if (strcmp(cmd, "!!") == 0) {          // if command is !!
             strcpy(cmd, lastcmd);   // make the current command the last command
+	    system(cmd); 	    // run dat bitch
         }
-            system(cmd);
+            system(cmd);	    // run dat bitch
     }
 }
